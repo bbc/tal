af986dd8c08f021b1b8f46773066099151f332a8
David Hurrell <david.hurrell@bbc.co.uk>
2013-04-16 17:23:00 +0100

    Add stubs for new setLocation(), getLocation() API

--- static/script/devices/device.js
+++ static/script/devices/device.js
@@ -527,6 +527,31 @@ require.def('antie/devices/device',
             getCurrentRoute: function() {
             },
             /**
+             * Launch a new application at the specified URL, passing in query string data and
+             * a route.
+             * @param {String} location The URL to launch (include protocol, host and port)
+             * @param {Object} data     Properties to pass in the query string. Property names are keys. Values are sent as strings. Use {undefined} as a value to send a valueless key.
+             * @param {Array}  route    Route for new application (a reference pointing to a new location within the application). @see getCurrentRoute(), @see setCurrentRoute()
+             */
+            setLocation: function(location, data, route) {
+            },
+            /**
+             * Return the URL of the current application, with no route or query string information -
+             * @see getCurrentRoute(), @see getLocationData().
+             * @returns {String} URL of the current application, including protocol, host and port.
+             */
+            getLocation: function() {
+            },
+            /**
+             * Returns the query string of the current application as an object containing properties with
+             * string values.
+             * Keys without values are represented by properties with the value 'undefined'. Empty string values are
+             * represented in the object as property with an empty string value.
+             * @returns {Object} Properties contained in the query string.
+             */
+            getLocationData: function() {
+            },
+            /**
              * Gets the reference (e.g. URL) of the resource that launched the application.
              * @returns A reference (e.g. URL) of the resource that launched the application.
              */
