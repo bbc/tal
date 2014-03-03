bec99fb12faf28e39a774a27f5662821f41da1cf
jcobbctv <jcobbctv@gmail.com>
2014-03-03 16:58:51 +0000

    removing some unwanted html5.js changes

--- static/script/devices/media/html5.js
+++ static/script/devices/media/html5.js
@@ -221,31 +221,7 @@ require.def(
             },
             // readonly attribute double duration;
             getDuration: function() {
-//                if( isFinite( this._mediaElement.duration ) ){
-//                    return this._mediaElement.duration;
-//                }else{
-                {
-                    var seekable = this._mediaElement.seekable;
-                    var nrRanges = seekable.length;
-
-                    var start,end;
-                    try{
-                        start = seekable.start( 0 );
-                    }
-                    catch( x ){
-                        console.log( x );
-                    }
-
-
-                    try{
-                        end = seekable.end( 0 );
-                    }
-                    catch( x ){
-                        console.log( x );
-                    }
-
-                    return end - start;
-                }
+                return this._mediaElement.duration;
             },
             // readonly attribute Date startOffsetTime;
             getStartOffsetTime: function() {
