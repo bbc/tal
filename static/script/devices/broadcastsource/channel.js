264e47de1452895d263b9d9314b54a605c785b8d
Jonathan Parkin <jonathan.parkin@bbc.co.uk>
2014-04-11 11:37:16 +0100

    TVPJSFRMWK-1454 - Added class representing channel information.

---
+++ static/script/devices/broadcastsource/channel.js
@@ -0,0 +1,60 @@
+/**
+ * @fileOverview Requirejs module containing the antie.devices.broadcastsource.channel class.
+ *
+ * @preserve Copyright (c) 2014 British Broadcasting Corporation
+ * (http://www.bbc.co.uk) and TAL Contributors (1)
+ *
+ * (1) TAL Contributors are listed in the AUTHORS file and at
+ *     https://github.com/fmtvp/TAL/AUTHORS - please extend this file,
+ *     not this notice.
+ *
+ * @license Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * All rights reserved
+ * Please contact us for an alternative licence
+ */
+
+require.def('antie/devices/broadcastsource/channel',
+    [
+        'antie/class'
+    ],
+    function (Class) {
+
+        return Class.extend(/** @lends antie.class.prototype */{
+            /**
+             * @constructor
+             * @param props.name channel name
+             * @param props.onid original network ID
+             * @param props.tsid transport stream ID
+             * @param props.sid program number
+             * @param props.type channel type
+             * @param props.ptc
+             * @param props.major
+             * @param props.minor
+             * @param props.sourceId
+             */
+            init : function(props) {
+                this.name = props.name;
+                this.type = props.type;
+                this.onid = props.onid || undefined;
+                this.tsid = props.tsid || undefined;
+                this.sid = props.sid || undefined;
+                this.ptc = props.ptc || undefined;
+                this.major = props.major || undefined;
+                this.minor = props.minor || undefined;
+                this.sourceId = props.sourceId || undefined;
+            }
+
+        });
+    }
+);
