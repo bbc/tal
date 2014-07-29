f4028aebaac91e8dacf9872feeb25a5beec36970
Tom Jenkinson <jenkit92@mc-s088510.local>
2014-07-29 17:15:38 +0100

    added comment blocks to helpers.js

--- static/script/widgets/label/texttruncation/helpers.js
+++ static/script/widgets/label/texttruncation/helpers.js
@@ -4,18 +4,30 @@ require.def('antie/widgets/label/texttruncation/helpers',
         "use strict";
 
         return {
-            // returns true if the result of str sliced to slicePoint would end at a word boundary
+            /**
+             * Determine whether the result of str sliced to slicePoint would end at a word boundary.
+             * @param {String} str The string that will be sliced.
+             * @param {String} slicePoint The point that this string would be sliced at. (Second param of slice)
+             * @returns true or false
+             */
             isAtWordBoundary: function(str, slicePoint) {
                 return slicePoint >= str.length || str[slicePoint] === " ";
             },
 
-            // returns the index of the last word boundary
+            /**
+             * Get the index of the start of the last word boundary in a string.
+             * @param {String} str The string to check.
+             * @returns The index or -1 if no word boundary could be found.
+             */
             getLastWordBoundaryIndex: function(str) {
                 return str.lastIndexOf(" ");
             },
 
-            // returns a trimmed version of str to the end of the last word
-            // this assumes that str is already finishing mid way through a word
+            /**
+             * Trim a string so that it ends after a complete word.
+             * @param {String} str The string to trim.
+             * @returns A trimmed version of the string.
+             */
             trimToWord: function(str) {
                 var lastSpaceIndex = this.getLastWordBoundaryIndex(str);
                 if (lastSpaceIndex !== -1) {
@@ -24,7 +36,11 @@ require.def('antie/widgets/label/texttruncation/helpers',
                 return str;
             },
 
-            // returns str with any trailing whitespace removed
+            /**
+             * Removes whitespace from the end of a string.
+             * @param {String} str The string to remove whitespace from.
+             * @returns The new string with trailing whitespace removed.
+             */
             trimTrailingWhitespace: function(str) {
                 while (str.length > 0 && str[str.length - 1] === " ") {
                     str = str.slice(0, str.length - 1);
@@ -32,7 +48,14 @@ require.def('antie/widgets/label/texttruncation/helpers',
                 return str;
             },
 
-            // perform a binary chop until find number of characters that will fit
+            /**
+             * Perform a binary chop to calculate the number of characters that fit into the WorkContainer.
+             * @param {antie.widgets.label.texttruncation.workcontainer} workContainer The WorkContainer.
+             * @param {String} txt The source string to work on.
+             * @param {String} txtEnd The text that would be added after the truncated text. E.g "...".
+             *                        The length of this is not included in the returned value.
+             * @returns The number of characters that fit in the WorkContainer.
+             */
             getNumCharactersThatFit: function(workContainer, txt, txtEnd) {
                 var positionGenerator = new PositionGenerator(txt.length);
                 var position = txt.length;
