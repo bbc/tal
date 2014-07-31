3248f193a56ff470139eddd17d37c7b24eca6623
Tom Jenkinson <jenkit92@mc-s088510.local>
2014-07-31 17:13:27 +0100

    tidied up tests for PositionGenerator

--- static/script-tests/tests/widgets/label/texttruncation/positiongenerator-test.js
+++ static/script-tests/tests/widgets/label/texttruncation/positiongenerator-test.js
@@ -10,17 +10,16 @@
         this.sandbox.restore();
     };
 
-    this.tests.prototype.testCheckThatPositionGeneratorReturnsCorrectNumberSequenceForDifferentLengthStrings = function (queue) {
-        expectAsserts(10 + 7 + 9 + 3 + 1 + 2 + 1);
+    this.tests.prototype.testCheckThatPositionGeneratorReturnsCorrectNumberSequence = function (queue) {
+        expectAsserts(9);
 
-        queuedRequire(queue, ["antie/widgets/label/texttruncation/positiongenerator"], function(PositionGenerator) {
+        queuedRequire(queue, ["antie/widgets/label/texttruncation/positiongenerator"], function (PositionGenerator) {
 
             var positionGenerator;
             positionGenerator = new PositionGenerator(8);
             assertEquals(true, positionGenerator.hasNext(true));
             assertEquals(4, positionGenerator.next(true));
             assertEquals(true, positionGenerator.hasNext(true));
-            assertEquals(2, positionGenerator.calculateNext(true));
             assertEquals(2, positionGenerator.next(true));
             assertEquals(true, positionGenerator.hasNext(true));
             assertEquals(1, positionGenerator.next(true));
@@ -28,38 +27,42 @@
             assertEquals(0, positionGenerator.next(true));
             assertEquals(false, positionGenerator.hasNext(true));
 
-            positionGenerator = new PositionGenerator(8);
-            assertEquals(true, positionGenerator.hasNext(true));
-            assertEquals(4, positionGenerator.next(true));
-            assertEquals(true, positionGenerator.hasNext(true));
-            assertEquals(2, positionGenerator.next(true));
-            assertEquals(true, positionGenerator.hasNext(false));
-            assertEquals(3, positionGenerator.next(false));
-            assertEquals(false, positionGenerator.hasNext(false));
+        });
+    };
 
-            positionGenerator = new PositionGenerator(8);
-            assertEquals(true, positionGenerator.hasNext(true));
-            assertEquals(4, positionGenerator.next(true));
-            assertEquals(true, positionGenerator.hasNext(true));
-            assertEquals(2, positionGenerator.next(true));
-            assertEquals(true, positionGenerator.hasNext(false));
-            assertEquals(3, positionGenerator.next(false));
-            assertEquals(true, positionGenerator.hasNext(true));
-            assertEquals(2, positionGenerator.next(true));
-            assertEquals(false, positionGenerator.hasNext(false));
+    this.tests.prototype.testCheckThatPositionGeneratorReturnsCorrectFirstPositionWithStringLengthIsNotAPowerOfTwo = function (queue) {
+        expectAsserts(2);
 
-            positionGenerator = new PositionGenerator(1);
-            assertEquals(true, positionGenerator.hasNext(true));
-            assertEquals(0, positionGenerator.next(true));
-            assertEquals(false, positionGenerator.hasNext(true));
+        queuedRequire(queue, ["antie/widgets/label/texttruncation/positiongenerator"], function (PositionGenerator) {
 
-            positionGenerator = new PositionGenerator(1);
-            assertEquals(false, positionGenerator.hasNext(false));
 
             positionGenerator = new PositionGenerator(9);
             assertEquals(true, positionGenerator.hasNext(true));
             assertEquals(1, positionGenerator.next(true));
 
+        });
+    }
+
+    this.tests.prototype.testCheckThatPositionGeneratorReturnsCorrectPositionAfterIsOverIsReportedFalseAndThenTrueAgainAndThenArrivesAtResult = function (queue) {
+        expectAsserts(5);
+
+        queuedRequire(queue, ["antie/widgets/label/texttruncation/positiongenerator"], function (PositionGenerator) {
+            positionGenerator = new PositionGenerator(8);
+            positionGenerator.next(true);
+            positionGenerator.next(true);
+            assertEquals(true, positionGenerator.hasNext(false));
+            assertEquals(3, positionGenerator.next(false));
+            assertEquals(true, positionGenerator.hasNext(true));
+            assertEquals(2, positionGenerator.next(true));
+            assertEquals(false, positionGenerator.hasNext(false));
+        });
+    };
+
+
+    this.tests.prototype.testCheckThatPositionGeneratorReturnsFalseFromHasNextWhenIsOverAndCheckingStringOfLengthZero = function (queue) {
+        expectAsserts(1);
+
+        queuedRequire(queue, ["antie/widgets/label/texttruncation/positiongenerator"], function (PositionGenerator) {
             positionGenerator = new PositionGenerator(0);
             assertEquals(false, positionGenerator.hasNext(true));
         });
