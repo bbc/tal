8565ed4aa0473e45734ba50a92d0ebdcb2d4483d
root <root@pal.sandbox>
2014-08-13 10:18:39 +0100

    Started work on parental guidance app handler

---
+++ static/script/devices/parentalguidance/pgchallengeresponse.js
@@ -0,0 +1,12 @@
+require.def('antie/devices/parentalguidance/pgchallengeresponse',
+	[],
+	function () {
+		'use strict';
+
+		return {
+			NOT_AUTHORISED: 0,
+			AUTHORISED: 1,
+			ERROR: 2,
+			CHALLENGE_NOT_SET: 3
+		};
+	});
