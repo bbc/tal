5e98a2de25e433aa4dec5207c930dc8d3977cb11
tsadlerBBC <thomas.sadler@bbc.co.uk>
2014-08-26 16:47:16 +0100

    samsung_maple_unload should use prototypal inheritance, not Class.extend, as per the other media device modifiers that extend exisiting media device modifiers

--- static/script/devices/media/samsung_maple_unload.js
+++ static/script/devices/media/samsung_maple_unload.js
@@ -2,16 +2,16 @@ require.def('antie/devices/media/samsung_maple_unload',
     [
       'antie/devices/media/samsung_maple'
     ],
-    function(SamsungMaple) {
+    function(SamsungMaplePlayer) {
 
-      return SamsungMaple.extend({
-        _addExitStrategyEventListener: function() {
+      SamsungMaplePlayer.prototype._addExitStrategyEventListener = function() {
           var self = this;
           window.addEventListener('unload', function () {
             self.playerPlugin.Stop();
             self.tvmwPlugin.SetSource(self.originalSource);
           }, false);
-        }
-      });
+        };
+
+        return SamsungMaplePlayer;
     }
 );
