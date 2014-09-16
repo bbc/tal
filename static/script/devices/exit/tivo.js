d713d5ba971eb54f6cf5c487caccc7c066c00128
Zia Choudary <zia-choudary@users.noreply.github.com>
2014-09-16 16:21:31 +0100

    Update tivo.js

--- static/script/devices/exit/tivo.js
+++ static/script/devices/exit/tivo.js
@@ -1,16 +1,1 @@
 
-require.def(
-    'antie/devices/exit/tivo',
-    ['antie/devices/browserdevice'],
-    function(Device) {
-        'use strict';
-
-        /**
-         * Exits the application by returning to the widget page using the tivo function.
-         */
-        Device.prototype.exit = function() {
-            tivo.core.exit();
-        };
-
-    }
-);
