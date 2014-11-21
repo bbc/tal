770be978575b9f581b11995ee7c4ece00db4eb50
Richard McIntyre <richard.mackstar@gmail.com>
2014-11-21 10:29:37 +0000

    Adding Grunt complexity

--- gruntfile.js
+++ gruntfile.js
@@ -23,9 +23,29 @@ module.exports = function (grunt) {
                 files: ["static/script/**/*.js"],
                 tasks: ["jshint"]
             }
+        },
+        complexity: {
+            generic: {
+                src: ['static/script/**/*.js'],
+                exclude: ['static/script/targets/core.js'],
+                expand: true,
+                options: {
+                    breakOnErrors: false,
+                    jsLintXML: 'report.xml',         // create XML JSLint-like report
+                    checkstyleXML: 'checkstyle.xml', // create checkstyle report
+                    errorsOnly: false,               // show only maintainability errors
+                    cyclomatic: [3, 7, 12],          // or optionally a single value, like 3
+                    halstead: [8, 13, 20],           // [difficulty, volume, effort] or optionally a single value, like 8
+                    maintainability: 100,
+                    hideComplexFunctions: false,     // only display maintainability
+                    broadcast: false                 // broadcast data over event-bus
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks("grunt-contrib-watch");
     grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks('grunt-complexity');
+    grunt.registerTask('default', 'complexity');
 };
