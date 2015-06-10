00d4ccb94e4b2e3fd454a28091a39b159c25d4af
Paul Caporn <caporp01@mc-s096162.local>
2015-06-10 11:06:36 +0100

    Exclude sagemcom from jshint

--- gruntfile.js
+++ gruntfile.js
@@ -8,13 +8,12 @@ module.exports = function (grunt) {
             files: ['static/script/**/*.js'],
             options: {
                 jshintrc: '.jshintrc',
-                // options here to override JSHint defaults
                 ignores: [
                     'static/script/lib/*',
                     'static/script/devices/googletv.js',
                     'static/script/devices/data/json2.js',
-                    'static/script/widgets/horizontalcarousel.js'
-
+                    'static/script/widgets/horizontalcarousel.js',
+                    'static/script/devices/exit/sagemcom.js'
                 ]
             }
         },
