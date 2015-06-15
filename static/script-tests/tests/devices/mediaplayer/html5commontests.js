e7763a3b0570065c9e2c930c8e99feeba7ad6697
Lewis Goodwin <Lewis.Goodwin@bbc.co.uk>
2015-06-15 09:25:32 +0100

    Added test to ensure enter buffering sentinel fires when begin playback is called and zeroes are trusted

--- static/script-tests/tests/devices/mediaplayer/html5commontests.js
+++ static/script-tests/tests/devices/mediaplayer/html5commontests.js
@@ -1288,6 +1288,29 @@ window.commonTests.mediaPlayer.html5.mixinTests = function (testCase, mediaPlaye
         });
     };
 
+    mixins.testEnterBufferingSentinelFiresWhenBeginPlaybackIsCalledAndDeviceTimeIsReportedAsZeroForAtLeastTwoIntervals = function (queue) {
+      expectAsserts(2);
+        var self = this;
+        runMediaPlayerTest(this, queue, function (MediaPlayer) {
+            getToPlayingWithBeginPlayback(self, MediaPlayer, 20);
+            clearEvents(self);
+
+            var i;
+            for (i = 0; i<3; i++) {
+                advancePlayTime(self);
+                fireSentinels(self);
+            }
+
+            for (i=0; i<2; i++) {
+                stubCreateElementResults.video.currentTime = 0;
+                fireSentinels(self);
+            }
+
+            assertNoEvent(self, MediaPlayer.EVENT.SENTINEL_ENTER_BUFFERING);
+            assertState(self, MediaPlayer.STATE.PLAYING);
+        });
+    };
+
     mixins.testEnterBufferingSentinelFiresWhenSeekedToZeroAndDeviceTimeIsReportedAsZeroForAtLeastTwoIntervals = function(queue) {
         expectAsserts(1);
         var self = this;
@@ -1339,7 +1362,7 @@ window.commonTests.mediaPlayer.html5.mixinTests = function (testCase, mediaPlaye
 
             assertEventTypeHasBeenFiredASpecificNumberOfTimes(self, MediaPlayer.EVENT.SENTINEL_ENTER_BUFFERING, 1);
         });
-    }
+    };
 
      mixins.testExitBufferingSentinelCausesTransitionToPlayingWhenPlaybackStarts = function(queue) {
         expectAsserts(3);
