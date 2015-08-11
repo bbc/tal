a34bf606f755448756d782dc3693fde9952fe981
tsadlerBBC <thomas.sadler@bbc.co.uk>
2015-08-11 14:05:54 +0100

    Wrapping strip tests now use jshint ignore:line instead of commented out variables where we want to retain unused variables

--- static/script-tests/tests/widgets/carousel/strips/wrappingstrip.js
+++ static/script-tests/tests/widgets/carousel/strips/wrappingstrip.js
@@ -344,7 +344,7 @@
                 'antie/widgets/carousel/orientations/vertical',
                 'antie/widgets/container'
             ],
-            function (application, WrappingStrip, Button, verticalOrientation /* Container */) {
+            function (application, WrappingStrip, Button, verticalOrientation, Container) { // jshint ignore:line
                 var strip, device;
                 device = application.getDevice();
                 self.sandbox.stub(device);
@@ -369,7 +369,7 @@
                 'antie/widgets/carousel/orientations/horizontal',
                 'antie/widgets/container'
             ],
-            function (application, WrappingStrip, Button, horizontalOrientation /* Container */) {
+            function (application, WrappingStrip, Button, horizontalOrientation, Container) { // jshint ignore:line
                 var strip, device;
                 device = application.getDevice();
                 self.sandbox.stub(device);
