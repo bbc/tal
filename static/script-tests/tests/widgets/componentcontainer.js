3827c059ff522513367e18a26f1d150a179c7c30
tsadlerBBC <thomas.sadler@bbc.co.uk>
2015-09-04 16:46:31 +0100

    Removed redundant queuedApplicationInit dependencies

--- static/script-tests/tests/widgets/componentcontainer.js
+++ static/script-tests/tests/widgets/componentcontainer.js
@@ -350,8 +350,8 @@
 		queuedApplicationInit(
 				queue,
 				"lib/mockapplication",
-				["antie/widgets/componentcontainer", "antie/widgets/component"],
-				function(application, ComponentContainer, Component) { // jshint ignore:line
+				["antie/widgets/componentcontainer"],
+				function(application, ComponentContainer) {
                     var clock = sinon.useFakeTimers();
 
                     var container = new ComponentContainer("container");
