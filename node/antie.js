2e7d3d75844050090b938acaf9bd24c8b198c9f5
Luke Preston <luke.preston@bbc.co.uk>
2016-02-22 11:09:38 +0000

    updating antie.js

--- node/antie.js
+++ node/antie.js
@@ -1,9 +1,27 @@
 var fs = require('fs')
 
+function fileExistsSync(path) {
+    try {
+        fs.lstatSync(path);
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+function fileExists(path, callback) {
+    try {
+        fs.lstat(path);
+        callback(true);
+    } catch (e) {
+        callback(false);
+    }
+}
+
 module.exports = {
-    getPageStrategyElement: function(pageStrategy, element) {
+    getPageStrategyElementSync: function(pageStrategy, element) {
         var pageStrategyPath = __dirname + '/../config/pagestrategy/' + pageStrategy + '/' + element;
-        if (!fs.existsSync(pageStrategyPath)) {
+        if (!fileExistsSync(pageStrategyPath)) {
             return {
                 noSuchStrategy: "file does not exist: " + pageStrategyPath
             }
@@ -11,5 +29,20 @@ module.exports = {
         return {
             data: fs.readFileSync(pageStrategyPath).toString()
         }
+    },
+    getPageStrategyElement: function(pageStrategy, element, callback) {
+        var pageStrategyPath = __dirname + '/../config/pagestrategy/' + pageStrategy + '/' + element;
+
+        fs.readFile(pageStrategyPath, function(err, data) {
+            if (err !== null) {
+                callback({
+                    noSuchStrategy: "file does not exist: " + pageStrategyPath
+                })
+            } else {
+                callback({
+                    data: data.toString()
+                })
+            }
+        })
     }
 }
