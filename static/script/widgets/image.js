88d40a1aa8d720723ad74ae940701fe9b58260e9
Gareth Smith <gareth.5.smith@bt.com>
2016-05-24 11:29:38 +0100

    Fix for Issue 360 - CSS classes not being added when image render mode was IMG

--- static/script/widgets/image.js
+++ static/script/widgets/image.js
@@ -39,19 +39,20 @@ define(
          * @extends antie.widgets.Container
          * @param {String} [id] The unique ID of the widget. If excluded, a temporary internal ID will be used (but not included in any output).
          * @param {String} src The image source URL.
-         * @param {Size} size The size of the image.
+         * @param {Size} [size] The size of the image.
+         * @param {Number} [renderMode] What mode the image will be rendered in. Either Image.RENDER_MODE_IMG or Image.RENDER_MODE_CONTAINER.
          */
         var Image = Container.extend(/** @lends antie.widgets.Image.prototype */ {
             /**
              * @constructor
              * @ignore
              */
-            init: function(id, src, size) {
+            init: function(id, src, size, renderMode) {
                 this._super(id);
                 this._src = src;
                 this._size = size;
                 this._imageElement = null;
-                this._renderMode = Image.RENDER_MODE_CONTAINER;
+                this._renderMode = (renderMode === Image.RENDER_MODE_IMG) ? Image.RENDER_MODE_IMG : Image.RENDER_MODE_CONTAINER;
                 this.addClass('image');
             },
             /**
@@ -69,6 +70,7 @@ define(
                     device.prependChildElement(this.outputElement, this._imageElement);
                 } else {
                     this.outputElement = this._imageElement;
+                    device.setElementClasses(this.outputElement, this.getClasses());
                 }
 
                 return this.outputElement;
