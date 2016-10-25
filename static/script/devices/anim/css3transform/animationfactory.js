1ba52fa56a403c3fdc9e80fb411abfa0ad238774
David Hurrell <david.hurrell@bbc.co.uk>
2016-10-25 12:00:52 +0100

    Add stub implementation for tweening opacity

--- static/script/devices/anim/css3transform/animationfactory.js
+++ static/script/devices/anim/css3transform/animationfactory.js
@@ -2,9 +2,10 @@ define(
     'antie/devices/anim/css3transform/animationfactory',
     [
         'antie/devices/anim/css3transform/translate',
-        'antie/devices/anim/css3transform/expand'
+        'antie/devices/anim/css3transform/expand',
+        'antie/devices/anim/css3transform/opacity'
     ],
-    function (Translate, Expand) {
+    function (Translate, Expand, Opacity) {
         'use strict';
 
         return function (options) {
@@ -16,6 +17,9 @@ define(
             }
             if (options.to.height && options.to.width) {
                 return new Expand(options);
+            }
+            if (options.to.opacity !== undefined) {
+                return new Opacity(options);
             }
         };
     }
---
+++ static/script/devices/anim/css3transform/opacity.js
@@ -0,0 +1,25 @@
+define(
+    'antie/devices/anim/css3transform/opacity',
+    [
+        'antie/devices/anim/shared/helpers'
+    ],
+    function (Helpers) {
+        'use strict';
+
+        return function (options) {
+            var el = options.el;
+
+            function start () {
+                Helpers.setStyle(el, 'opacity', options.to.opacity);
+            }
+
+            function stop () {
+            }
+
+            return {
+                start: start,
+                stop: stop
+            };
+        };
+    }
+);
