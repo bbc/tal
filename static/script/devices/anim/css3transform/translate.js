83858179d80ccc6b8515812f83139bfb76b24a6c
David Hurrell <david.hurrell@bbc.co.uk>
2016-10-26 16:31:21 +0100

    Reposition element after animating via translate3d()
    Ensures that other elements on the page are affected by this element's new
    position as they should be. Removing the CSS transform property also
    ensures that the element becomes part of the same layer as the rest of the
    page after animating, which avoids idiosyncracies with positioning and
    z-indexing of child elements, etc.

--- static/script/devices/anim/css3transform/translate.js
+++ static/script/devices/anim/css3transform/translate.js
@@ -10,29 +10,48 @@ define(
             var el = options.el;
             var onTransitionEnd;
 
+            function getStyle (name) {
+                var value = parseInt(el.style[name], 10);
+                return value || 0;
+            }
+
+            function getDifference (direction) {
+                return position - getStyle(direction);
+            }
+
             function transform () {
                 if (axis === 'X') {
-                    Helpers.setStyle(el, 'transform', 'translate3d(' + position + 'px, 0, 0)', true);
+                    Helpers.setStyle(el, 'transform', 'translate3d(' + getDifference('left') + 'px, 0, 0)', true);
                 } else {
-                    Helpers.setStyle(el, 'transform', 'translate3d(0, ' + position + 'px, 0)', true);
+                    Helpers.setStyle(el, 'transform', 'translate3d(0, ' + getDifference('top') + 'px, 0)', true);
                 }
+            }
+
+            function endTransform () {
+                var axis2Direction = {
+                    X: 'left',
+                    Y: 'top'
+                };
+
+                el.classList.remove('animate');
+                Helpers.setStyle(el, 'transform', '', true);
+                Helpers.setStyle(el, axis2Direction[axis], position + 'px', false);
+
+                options.onComplete();
             }
 
             function start () {
                 if (Helpers.skipAnim(options)) {
-                    el.classList.remove('animate');
-                    transform();
-                    options.onComplete();
+                    endTransform();
                     return;
                 }
 
                 el.classList.add('animate');
-                onTransitionEnd = Helpers.registerTransitionEndEvent(el, options.onComplete);
+                onTransitionEnd = Helpers.registerTransitionEndEvent(el, endTransform);
                 transform();
             }
 
             function stop () {
-                el.classList.remove('animate');
                 onTransitionEnd();
             }
 
