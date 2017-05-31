ec081d1c81d93c36ed97e0cfd1de9e8d4f8f61f7
Adam Walters <adam.walters@bbc.co.uk>
2017-05-31 09:01:11 +0100

    Create stub test file for ps3 base

---
+++ static/script-tests/tests/devices/ps3basetest.js
@@ -0,0 +1,25 @@
+/**
+ * @preserve Copyright (c) 2013-present British Broadcasting Corporation. All rights reserved.
+ * @license See https://github.com/fmtvp/tal/blob/master/LICENSE for full licence
+ */
+
+require(
+  [
+    'antie/devices/ps3base',
+  ],
+  function (PS3Base) {
+    'use strict';
+
+    describe('antie.devices.PS3Base', function () {
+      var device;
+
+      beforeEach(function () {
+        device = new PS3Base(antie.framework.deviceConfiguration);
+      });
+
+      it('should expose nativeCallback as a method', function () {
+        expect(typeof device.nativeCallback).toEqual('function');
+        debugger;
+      });
+    })
+  });
