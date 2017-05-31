209fbdc5fb55ed9a7fe90acec0c9c45fc764468d
Adam Walters <adam.walters@bbc.co.uk>
2017-05-31 09:09:00 +0100

    Add stub blocks for all ps3base methods

--- static/script-tests/tests/devices/ps3basetest.js
+++ static/script-tests/tests/devices/ps3basetest.js
@@ -19,7 +19,26 @@ require(
 
       it('should expose nativeCallback as a method', function () {
         expect(typeof device.nativeCallback).toEqual('function');
-        debugger;
+      });
+
+      it('should expose addNativeEventListener as a method', function() {
+        expect(typeof device.addNativeEventListener).toEqual('function');
+      });
+
+      it('should expose removeNativeEventListener as a method', function() {
+        expect(typeof device.removeNativeEventListener).toEqual('function');
+      });
+
+      it('should expose nativeCommand as a method', function() {
+        expect(typeof device.nativeCommand).toEqual('function');
+      });
+
+      it('should expose loadAuthenticatedURL as a method', function() {
+        expect(typeof device.loadAuthenticatedURL).toEqual('function');
+      });
+
+      it('should expose isHDEnabled as a method', function() {
+        expect(typeof device.isHDEnabled).toEqual('function');
       });
     })
   });
