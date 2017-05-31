56acb8dda8b6faeff2c05aa46eb054e01dc28c45
John Beech <john.beech@bbc.co.uk>
2017-05-31 09:21:28 +0100

    Fix linting

--- static/script-tests/tests/devices/ps3basetest.js
+++ static/script-tests/tests/devices/ps3basetest.js
@@ -4,41 +4,41 @@
  */
 
 require(
-  [
-    'antie/devices/ps3base',
-  ],
-  function (PS3Base) {
-    'use strict';
+    [
+        'antie/devices/ps3base'
+    ],
+    function (PS3Base) {
+        'use strict';
 
-    describe('antie.devices.PS3Base', function () {
-      var device;
+        describe('antie.devices.PS3Base', function () {
+            var device;
 
-      beforeEach(function () {
-        device = new PS3Base(antie.framework.deviceConfiguration);
-      });
+            beforeEach(function () {
+                device = new PS3Base(antie.framework.deviceConfiguration);
+            });
 
-      it('should expose nativeCallback as a method', function () {
-        expect(typeof device.nativeCallback).toEqual('function');
-      });
+            it('should expose nativeCallback as a method', function () {
+                expect(typeof device.nativeCallback).toEqual('function');
+            });
 
-      it('should expose addNativeEventListener as a method', function() {
-        expect(typeof device.addNativeEventListener).toEqual('function');
-      });
+            it('should expose addNativeEventListener as a method', function () {
+                expect(typeof device.addNativeEventListener).toEqual('function');
+            });
 
-      it('should expose removeNativeEventListener as a method', function() {
-        expect(typeof device.removeNativeEventListener).toEqual('function');
-      });
+            it('should expose removeNativeEventListener as a method', function () {
+                expect(typeof device.removeNativeEventListener).toEqual('function');
+            });
 
-      it('should expose nativeCommand as a method', function() {
-        expect(typeof device.nativeCommand).toEqual('function');
-      });
+            it('should expose nativeCommand as a method', function () {
+                expect(typeof device.nativeCommand).toEqual('function');
+            });
 
-      it('should expose loadAuthenticatedURL as a method', function() {
-        expect(typeof device.loadAuthenticatedURL).toEqual('function');
-      });
+            it('should expose loadAuthenticatedURL as a method', function () {
+                expect(typeof device.loadAuthenticatedURL).toEqual('function');
+            });
 
-      it('should expose isHDEnabled as a method', function() {
-        expect(typeof device.isHDEnabled).toEqual('function');
-      });
-    })
-  });
+            it('should expose isHDEnabled as a method', function () {
+                expect(typeof device.isHDEnabled).toEqual('function');
+            });
+        });
+    });
